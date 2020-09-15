ee6440428ef47f2b444f108ad758867905a8ffcf
Victor Quiroz <victorhqc@gmail.com>
2020-09-15 13:27:10 +0200

    fix: correct config

--- photo-api/src/main.rs
+++ photo-api/src/main.rs
@@ -1,12 +1,10 @@
-
 #[macro_use]
 extern crate gotham_derive;
 
 use dotenv::dotenv;
 
-
 fn main() {
-    #[cfg(target_family = "linux")]
+    #[cfg(target_os = "linux")]
     openssl_probe::init_ssl_cert_env_vars();
 
     dotenv().ok();
