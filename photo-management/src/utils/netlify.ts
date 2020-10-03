63f6654b768e56577cd5d40097808be4a1f21364
Victor Quiroz <victorhqc@gmail.com>
2020-10-03 17:34:36 +0200

    fix: correct name for cra env variable

--- photo-management/src/utils/netlify.ts
+++ photo-management/src/utils/netlify.ts
@@ -1,5 +1,5 @@
 export async function rebuildApplication() {
-  const url = process.env.BUILD_HOOK_URL || 'build-hook-url-not-set';
+  const url = process.env.REACT_APP_BUILD_HOOK_URL || 'build-hook-url-not-set';
   await fetch(url, {
     method: 'POST',
   });
