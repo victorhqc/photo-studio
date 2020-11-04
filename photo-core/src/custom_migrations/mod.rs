bdd32451e0e8f61ddb9883abd3ccb1f9ef53e8aa
Victor Quiroz <victorhqc@gmail.com>
2020-11-04 14:19:21 +0100

    feat: naive migration
    Quite slow but it seems to be working

--- photo-core/src/custom_migrations/mod.rs
+++ photo-core/src/custom_migrations/mod.rs
@@ -2,6 +2,7 @@ use crate::connection::{connect, Conn};
 use crate::helpers::uuid::Uuid;
 use crate::models::{Album, ModelError, User};
 use crate::schema::custom_migrations;
+use crate::schema::photos;
 use chrono::naive::serde::ts_seconds;
 use chrono::NaiveDateTime;
 use chrono::Utc;
@@ -10,7 +11,11 @@ use serde::{Deserialize, Serialize};
 use snafu::{Backtrace, ResultExt};
 
 lazy_static! {
-    static ref MIGRATIONS: Vec<String> = [String::from("lifestyle_album"),].to_vec();
+    static ref MIGRATIONS: Vec<String> = [
+        String::from("lifestyle_album"),
+        String::from("image_metadata")
+    ]
+    .to_vec();
 }
 
 pub fn apply_custom_migrations(url: Option<String>) -> Result<()> {
@@ -36,6 +41,7 @@ pub fn apply_custom_migrations(url: Option<String>) -> Result<()> {
     to_apply.iter().for_each(|name| {
         match &name[..] {
             "lifestyle_album" => migrate_lifestyle_album(&conn).unwrap(),
+            "image_metadata" => migrate_image_metadata(&conn).unwrap(),
             _ => {}
         };
     });
@@ -60,6 +66,52 @@ fn migrate_lifestyle_album(conn: &Conn) -> Result<()> {
 
     let migration = CustomMigration::new("lifestyle_album".to_string());
     migration.insert(&conn)?;
+
+    Ok(())
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, AsChangeset)]
+#[table_name = "photos"]
+struct UpdatePhoto {
+    pub width: i32,
+    pub height: i32,
+}
+
+fn migrate_image_metadata(conn: &Conn) -> Result<()> {
+    debug!("Migrating image_metadata");
+
+    let users = User::find_all(&conn).context(Model)?;
+
+    users.iter().for_each(|user| {
+        let albums = Album::find_all(conn, &user).unwrap();
+        albums.iter().for_each(|(album, _)| {
+            let photos = album.photos(conn).unwrap();
+
+            photos.iter().for_each(|photo| {
+                let img_bytes = reqwest::blocking::get(&photo.src).unwrap().bytes().unwrap();
+                let image = image::load_from_memory(&img_bytes).unwrap().to_bgr();
+
+                {
+                    use crate::schema::photos::dsl::*;
+
+                    let updated = UpdatePhoto {
+                        width: image.width() as i32,
+                        height: image.height() as i32,
+                    };
+
+                    debug!("Updating image dimensions {:?}", updated);
+
+                    diesel::update(photos)
+                        .filter(id.eq(photo.id))
+                        .set(updated)
+                        .execute(conn)
+                        .unwrap();
+                };
+            });
+        });
+    });
+
+    debug!("Migration ended!");
 
     Ok(())
 }
